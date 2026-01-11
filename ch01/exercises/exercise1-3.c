0c81b8026177548b3a0a9861cca6a8d670fc2c07
Phil Dexter <phildexter86@gmail.com>
2026-01-11 15:07:34 -0500

    Added exercise1-3.c

---
+++ ch01/exercises/exercise1-3.c
@@ -0,0 +1,26 @@
+/* exercise1-3.c
+ * 01/11/2026
+ * Modify the temp conversion program to print a heading above the table.
+ */
+
+ #include <stdio.h>
+
+/* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300;
+   floating point version*/
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0; // lower limit of temperature table
+    upper = 300; // upper limit 
+    step = 20;   // step size
+
+    fahr = lower;
+    printf("fahr celsius\n");
+    while(fahr <= upper){
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
