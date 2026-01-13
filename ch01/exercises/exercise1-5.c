463cfc74d3506939a6f6c457dc1430a86aa12ba0
Phil Dexter <phildexter86@gmail.com>
2026-01-13 10:18:47 -0500

    Added exercise1-5.c

---
+++ ch01/exercises/exercise1-5.c
@@ -0,0 +1,23 @@
+/*
+ * exercise1-5.c
+ * 01/13/2026
+ * Modify the temperature conversion program to print the table
+ * in reverse order; from 300 degrees to 0.
+ * 
+*/
+
+
+/* print Fahrenheit-Celsius table*/
+#include <stdio.h>
+
+int main(void)
+{
+    int fahr;
+
+    for (fahr = 300; fahr >= 0; fahr = fahr -= 20){
+        printf("%3d%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+    }
+
+    return 0;
+}
